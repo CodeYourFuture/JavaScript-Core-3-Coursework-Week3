7cd9ce9e6cf2e6b8f4dfbfe07985a27bfb4da5a4
Cecilia <earwen1986@gmail.com>
2021-02-27 17:54:17 +0000

    Fixed some errors in exercise 2

--- exercises/exercise-2/exercise-2.js
+++ exercises/exercise-2/exercise-2.js
@@ -1,24 +1,85 @@
 let hogwarts = [
-    { firstName: "Harry", lastName: "Potter", house: "Gryffindor", pet: "Owl", occupation: "Student" },
-    { firstName: "Hermione", lastName: "Granger", house: "Gryffindor", pet: "Cat", occupation: "Student" },
-    { firstName: "Draco", lastName: "Malfoy", house: "Slytherin", pet: null, occupation: "Student" },
-    { firstName: "Cedric", lastName: "Diggory", house: "HufflePuff", pet: null, occupation: "Student" },
-    { firstName: "Severus", lastName: "Snape", house: "Slytherin", pet: null, occupation: "Teacher" },
-    { firstName: "Filius", lastName: "Flitwick", house: "Ravenclaw", pet: null, occupation: "Teacher" },
-    { firstName: "Pomona", lastName: "Sprout", house: "Hufflepuff", pet: null, occupation: "Teacher" },
-    { firstName: "Minerva", lastName: "McGonagall", house: "Gryffindor", pet: null, occupation: "Teacher" },
-    { firstName: "Albus", lastName: "Dumbledore", house: "Gryffindor", pet: "Phoenix", occupation: "Teacher" }
-]
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    house: "Gryffindor",
+    pet: "Owl",
+    occupation: "Student",
+  },
+  {
+    firstName: "Ron",
+    lastName: "Weasley",
+    house: "Gryffindor",
+    pet: "Rat",
+    occupation: "Student",
+  },
+  {
+    firstName: "Hermione",
+    lastName: "Granger",
+    house: "Gryffindor",
+    pet: "Cat",
+    occupation: "Student",
+  },
+  {
+    firstName: "Draco",
+    lastName: "Malfoy",
+    house: "Slytherin",
+    pet: null,
+    occupation: "Student",
+  },
+  {
+    firstName: "Cedric",
+    lastName: "Diggory",
+    house: "HufflePuff",
+    pet: null,
+    occupation: "Student",
+  },
+  {
+    firstName: "Severus",
+    lastName: "Snape",
+    house: "Slytherin",
+    pet: null,
+    occupation: "Teacher",
+  },
+  {
+    firstName: "Filius",
+    lastName: "Flitwick",
+    house: "Ravenclaw",
+    pet: null,
+    occupation: "Teacher",
+  },
+  {
+    firstName: "Pomona",
+    lastName: "Sprout",
+    house: "Hufflepuff",
+    pet: null,
+    occupation: "Teacher",
+  },
+  {
+    firstName: "Minerva",
+    lastName: "McGonagall",
+    house: "Gryffindor",
+    pet: null,
+    occupation: "Teacher",
+  },
+  {
+    firstName: "Albus",
+    lastName: "Dumbledore",
+    house: "Gryffindor",
+    pet: "Phoenix",
+    occupation: "Teacher",
+  },
+];
 
 // Task 1
 
 function findGriffindor(arr) {
-	for (i of arr) {
-		const {firstName, lastName, house } = i;
-		if (house === "Gryffindor") {
-			console.log(firstName, lastName);
-		}
-	}
+  for (i of arr) {
+    const { firstName, lastName, house } = i;
+    if (house === "Gryffindor") {
+      console.log(firstName, lastName);
+    }
+  }
 }
 
 findGriffindor(hogwarts);
@@ -27,12 +88,12 @@ findGriffindor(hogwarts);
 console.log();
 
 function findPets(arr) {
-	for (i of arr) {
-		const {firstName, lastName, pet} = i;
-		if (pet) {
-			console.log(firstName, lastName);
-		}
-	}
+  for (i of arr) {
+    const { firstName, lastName, pet, occupation } = i;
+    if (occupation === "Teacher" && pet) {
+      console.log(firstName, lastName);
+    }
+  }
 }
 
 findPets(hogwarts);
