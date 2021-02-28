70694ff303f51ce7f34743ef4458e36e4cbe35b4
Michael <antenehnigussie1@yahoo.com>
2021-02-28 13:40:17 +0000

    Complete the exercise.

--- exercises/exercise-2/exercise-2.js
+++ exercises/exercise-2/exercise-2.js
@@ -1,11 +1,92 @@
 let hogwarts = [
-    { firstName: "Harry", lastName: "Potter", house: "Gryffindor", pet: "Owl", occupation: "Student" },
-    { firstName: "Hermione", lastName: "Granger", house: "Gryffindor", pet: "Cat", occupation: "Student" },
-    { firstName: "Draco", lastName: "Malfoy", house: "Slytherin", pet: null, occupation: "Student" },
-    { firstName: "Cedric", lastName: "Diggory", house: "HufflePuff", pet: null, occupation: "Student" },
-    { firstName: "Severus", lastName: "Snape", house: "Slytherin", pet: null, occupation: "Teacher" },
-    { firstName: "Filius", lastName: "Flitwick", house: "Ravenclaw", pet: null, occupation: "Teacher" },
-    { firstName: "Pomona", lastName: "Sprout", house: "Hufflepuff", pet: null, occupation: "Teacher" },
-    { firstName: "Minerva", lastName: "McGonagall", house: "Gryffindor", pet: null, occupation: "Teacher" },
-    { firstName: "Albus", lastName: "Dumbledore", house: "Gryffindor", pet: "Phoenix", occupation: "Teacher" }
-]
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    house: "Gryffindor",
+    pet: "Owl",
+    occupation: "Student",
+  },
+  {
+    firstName: "Ron",
+    lastName: "Weasley",
+    house: "Gryffindor",
+    pet: null,
+    occupation: "Student",
+  },
+  {
+    firstName: "Hermione",
+    lastName: "Granger",
+    house: "Gryffindor",
+    pet: "Cat",
+    occupation: "Student",
+  },
+  {
+    firstName: "Draco",
+    lastName: "Malfoy",
+    house: "Slytherin",
+    pet: null,
+    occupation: "Student",
+  },
+  {
+    firstName: "Cedric",
+    lastName: "Diggory",
+    house: "HufflePuff",
+    pet: null,
+    occupation: "Student",
+  },
+  {
+    firstName: "Severus",
+    lastName: "Snape",
+    house: "Slytherin",
+    pet: null,
+    occupation: "Teacher",
+  },
+  {
+    firstName: "Filius",
+    lastName: "Flitwick",
+    house: "Ravenclaw",
+    pet: null,
+    occupation: "Teacher",
+  },
+  {
+    firstName: "Pomona",
+    lastName: "Sprout",
+    house: "Hufflepuff",
+    pet: null,
+    occupation: "Teacher",
+  },
+  {
+    firstName: "Minerva",
+    lastName: "McGonagall",
+    house: "Gryffindor",
+    pet: null,
+    occupation: "Teacher",
+  },
+  {
+    firstName: "Albus",
+    lastName: "Dumbledore",
+    house: "Gryffindor",
+    pet: "Phoenix",
+    occupation: "Teacher",
+  },
+];
+
+// Task 1 Solution
+const houseOfGryffindor = (character) => {
+  if (character) {
+    // if character is not undefined
+    return `${character.firstName} ${character.lastName}`;
+  }
+};
+
+const [Harry, Ron, Hermione, , , , , , Minerva, Albus] = hogwarts;
+[Harry, Ron, Hermione, , , , , , Minerva, Albus].forEach((ch) =>
+  console.log(houseOfGryffindor(ch))
+);
+
+// Task 2 Solution
+const charactersWithPets = ([Albus, ...rest]) => {
+  console.log(`\n${Albus.firstName} ${Albus.lastName}`);
+};
+
+charactersWithPets(hogwarts.reverse()); // Albus Dumbledore (the last item) now becomes the first in the list
