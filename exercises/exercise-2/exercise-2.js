acded53c8d860dda561e26b564d5db33f214edfc
Amanuel <weditedi25@gmail.com>
2021-03-01 15:18:36 +0000

    Exe 1 and 2 done

--- exercises/exercise-2/exercise-2.js
+++ exercises/exercise-2/exercise-2.js
@@ -1,11 +1,86 @@
 let hogwarts = [
-    { firstName: "Harry", lastName: "Potter", house: "Gryffindor", pet: "Owl", occupation: "Student" },
-    { firstName: "Hermione", lastName: "Granger", house: "Gryffindor", pet: "Cat", occupation: "Student" },
-    { firstName: "Draco", lastName: "Malfoy", house: "Slytherin", pet: null, occupation: "Student" },
-    { firstName: "Cedric", lastName: "Diggory", house: "HufflePuff", pet: null, occupation: "Student" },
-    { firstName: "Severus", lastName: "Snape", house: "Slytherin", pet: null, occupation: "Teacher" },
-    { firstName: "Filius", lastName: "Flitwick", house: "Ravenclaw", pet: null, occupation: "Teacher" },
-    { firstName: "Pomona", lastName: "Sprout", house: "Hufflepuff", pet: null, occupation: "Teacher" },
-    { firstName: "Minerva", lastName: "McGonagall", house: "Gryffindor", pet: null, occupation: "Teacher" },
-    { firstName: "Albus", lastName: "Dumbledore", house: "Gryffindor", pet: "Phoenix", occupation: "Teacher" }
-]
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    house: "Gryffindor",
+    pet: "Owl",
+    occupation: "Student",
+  },
+  {
+    firstName: "Hermione",
+    lastName: "Granger",
+    house: "Gryffindor",
+    pet: "Cat",
+    occupation: "Student",
+  },
+  {
+    firstName: "Draco",
+    lastName: "Malfoy",
+    house: "Slytherin",
+    pet: null,
+    occupation: "Student",
+  },
+  {
+    firstName: "Cedric",
+    lastName: "Diggory",
+    house: "HufflePuff",
+    pet: null,
+    occupation: "Student",
+  },
+  {
+    firstName: "Severus",
+    lastName: "Snape",
+    house: "Slytherin",
+    pet: null,
+    occupation: "Teacher",
+  },
+  {
+    firstName: "Filius",
+    lastName: "Flitwick",
+    house: "Ravenclaw",
+    pet: null,
+    occupation: "Teacher",
+  },
+  {
+    firstName: "Pomona",
+    lastName: "Sprout",
+    house: "Hufflepuff",
+    pet: null,
+    occupation: "Teacher",
+  },
+  {
+    firstName: "Minerva",
+    lastName: "McGonagall",
+    house: "Gryffindor",
+    pet: null,
+    occupation: "Teacher",
+  },
+  {
+    firstName: "Albus",
+    lastName: "Dumbledore",
+    house: "Gryffindor",
+    pet: "Phoenix",
+    occupation: "Teacher",
+  },
+];
+
+// let [firstPerson, secondPerson, thirdPerson, fourthPerson, fifthPerson, sixthPerson, seventhPerson, eighthPerson, ninthPerson] = hogwarts;
+// let people = [firstPerson, secondPerson, thirdPerson, fourthPerson, fifthPerson, sixthPerson, seventhPerson, eighthPerson, ninthPerson];
+let [firstPerson, secondPerson, ...restOfPeople] = hogwarts;
+let people = [firstPerson, secondPerson, ...restOfThePeople];
+
+//Task 1
+people.forEach((person) => {
+  if (person.house === "Gryffindor") {
+    console.log(`${person.firstName} ${person.lastName}`);
+  }
+});
+
+//Task 2
+people.forEach((person) => {
+  if (person.occupation === "Teacher" && person.pet !== null) {
+    console.log(`${person.firstName} ${person.lastName}`);
+  }
+});
+
+// console.log(people);
--- exercises/exercise-3/exercise-3.js
+++ exercises/exercise-3/exercise-3.js
@@ -1,9 +1,18 @@
- let order = [
-     { itemName: "Hot cakes", quantity: 1, unitPrice: 2.29},
-     { itemName: "Apple Pie", quantity: 2, unitPrice: 1.39},
-     { itemName: "Egg McMuffin", quantity: 1, unitPrice: 2.80},
-     { itemName: "Sausage McMuffin", quantity: 1, unitPrice: 3.00},
-     { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.00},
-     { itemName: "Hash Brown", quantity: 4, unitPrice: 0.40}
- ]
- 
+let order = [
+  {itemName: "Hot cakes", quantity: 1, unitPrice: 2.29},
+  {itemName: "Apple Pie", quantity: 2, unitPrice: 1.39},
+  {itemName: "Egg McMuffin", quantity: 1, unitPrice: 2.8},
+  {itemName: "Sausage McMuffin", quantity: 1, unitPrice: 3.0},
+  {itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0},
+  {itemName: "Hash Brown", quantity: 4, unitPrice: 0.4},
+];
+
+let [item1, item2, item3, item4, item5, item6] = order;
+let items = [item1, item2, item3, item4, item5, item6];
+let total = 0;
+
+console.table(items);
+items.forEach((item) => {
+  total = total + item.unitPrice * item.quantity;
+});
+console.log(`Total: ${total}`);
