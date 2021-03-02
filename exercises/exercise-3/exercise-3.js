252bd2878ebcfd78f2ade4c2f1e5e996c5ad3809
ltteh <67978926+ltteh@users.noreply.github.com>
2021-03-02 00:28:07 +0000

    Completed

--- exercises/exercise-3/exercise-3.js
+++ exercises/exercise-3/exercise-3.js
@@ -1,9 +1,32 @@
- let order = [
-     { itemName: "Hot cakes", quantity: 1, unitPrice: 2.29},
-     { itemName: "Apple Pie", quantity: 2, unitPrice: 1.39},
-     { itemName: "Egg McMuffin", quantity: 1, unitPrice: 2.80},
-     { itemName: "Sausage McMuffin", quantity: 1, unitPrice: 3.00},
-     { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.00},
-     { itemName: "Hash Brown", quantity: 4, unitPrice: 0.40}
- ]
- 
+let order = [
+  { itemName: "Hot cakes", quantity: 1, unitPrice: 2.29 },
+  { itemName: "Apple Pie", quantity: 2, unitPrice: 1.39 },
+  { itemName: "Egg McMuffin", quantity: 1, unitPrice: 2.8 },
+  { itemName: "Sausage McMuffin", quantity: 1, unitPrice: 3.0 },
+  { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
+  { itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
+];
+
+const { 
+  itemName, 
+  quantity, 
+  unitPrice 
+} = order;
+
+let sum = 0;
+
+function receipt(data) {
+  let total = 0;
+
+  console.log(`QTY\t\tITEM\t\t\t\tTOTAL`);
+
+  data.forEach((element) => {
+    total = (element.unitPrice * element.quantity).toPrecision(3);
+    sum += parseFloat(total);
+    console.log(`${element.quantity}\t\t${element.itemName}\t\t\t${total}`);
+  });
+
+  console.log(`\nTotal: ${sum}`);
+}
+
+receipt(order);
--- practice/code-reading.md
+++ practice/code-reading.md
@@ -16,6 +16,9 @@ Take a look at the following code:
 
 Explain why line 4 and line 6 output different numbers.
 
+output x in line 5 is x declared in line 4 [a local variable (scopes) which only accessible within the function f1 ()]
+while output x in line 7 is x declared in line 1
+
 ## Question 2
 
 Take a look at the following code:
@@ -34,7 +37,7 @@ console.log(y)
 ```
 
 What will be the output of this code. Explain your answer in 50 words or less.
-
+When console function f1() executed, first output is 10 that is declared by x in global scope, however when console y executed, the output is undefined with reference error, this is because y is declared inside function f1 (local scope), but the execution is outside.
 ## Question 3
 
 Take a look at the following code:
@@ -62,3 +65,5 @@ console.log(y);
 ```
 
 What will be the output of this code. Explain your answer in 50 words or less.
+Even though val return is 10, however it cannot change x's value because const x's value cannot be changed. Hence x output is 9.
+It's different for const y because the declare value is an object property, meaning the object's value for y can be changed. Hence the y output is 10.
