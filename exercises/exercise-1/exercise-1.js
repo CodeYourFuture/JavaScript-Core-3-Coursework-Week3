3a4ec11f4a969ec85b1586dcd24280989a1dc580
Bahia369 <hsbcoding@gmail.com>
2021-03-03 18:18:41 +0000

    js-c3-w3
    exercises complete

--- exercises/exercise-1/exercise-1.js
+++ exercises/exercise-1/exercise-1.js
@@ -1,11 +1,24 @@
-const personOne = {
-    name: 'Popeye',
-    age: 34,
-    favouriteFood: 'Spinach'
-}
+// const personOne = {
+//     name: 'Popeye',
+//     age: 34,
+//     favouriteFood: 'Spinach'
+// }
 
-function introduceYourself(___________________________) {
-    console.log (`Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`); 
+// function introduceYourself(___________________________) {
+//     console.log(`Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`);
+// }
+
+// introduceYourself(personOne);
+const personOne = {
+    name: "Popeye",
+    age: 34,
+    favouriteFood: "Spinach",
+};
+
+function introduceYourself({ name, age, favouriteFood } = personOne) {
+    console.log(
+        `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
+    );
 }
 
 introduceYourself(personOne);
--- exercises/exercise-2/exercise-2.js
+++ exercises/exercise-2/exercise-2.js
@@ -1,11 +1,88 @@
-let hogwarts = [
-    { firstName: "Harry", lastName: "Potter", house: "Gryffindor", pet: "Owl", occupation: "Student" },
-    { firstName: "Hermione", lastName: "Granger", house: "Gryffindor", pet: "Cat", occupation: "Student" },
-    { firstName: "Draco", lastName: "Malfoy", house: "Slytherin", pet: null, occupation: "Student" },
-    { firstName: "Cedric", lastName: "Diggory", house: "HufflePuff", pet: null, occupation: "Student" },
-    { firstName: "Severus", lastName: "Snape", house: "Slytherin", pet: null, occupation: "Teacher" },
-    { firstName: "Filius", lastName: "Flitwick", house: "Ravenclaw", pet: null, occupation: "Teacher" },
-    { firstName: "Pomona", lastName: "Sprout", house: "Hufflepuff", pet: null, occupation: "Teacher" },
-    { firstName: "Minerva", lastName: "McGonagall", house: "Gryffindor", pet: null, occupation: "Teacher" },
-    { firstName: "Albus", lastName: "Dumbledore", house: "Gryffindor", pet: "Phoenix", occupation: "Teacher" }
-]
+let hogwarts = [{
+        firstName: "Harry",
+        lastName: "Potter",
+        house: "Gryffindor",
+        pet: "Owl",
+        occupation: "Student",
+    },
+    {
+        firstName: "Hermione",
+        lastName: "Granger",
+        house: "Gryffindor",
+        pet: "Cat",
+        occupation: "Student",
+    },
+    {
+        firstName: "Draco",
+        lastName: "Malfoy",
+        house: "Slytherin",
+        pet: null,
+        occupation: "Student",
+    },
+    {
+        firstName: "Cedric",
+        lastName: "Diggory",
+        house: "HufflePuff",
+        pet: null,
+        occupation: "Student",
+    },
+    {
+        firstName: "Severus",
+        lastName: "Snape",
+        house: "Slytherin",
+        pet: null,
+        occupation: "Teacher",
+    },
+    {
+        firstName: "Filius",
+        lastName: "Flitwick",
+        house: "Ravenclaw",
+        pet: null,
+        occupation: "Teacher",
+    },
+    {
+        firstName: "Pomona",
+        lastName: "Sprout",
+        house: "Hufflepuff",
+        pet: null,
+        occupation: "Teacher",
+    },
+    {
+        firstName: "Minerva",
+        lastName: "McGonagall",
+        house: "Gryffindor",
+        pet: null,
+        occupation: "Teacher",
+    },
+    {
+        firstName: "Albus",
+        lastName: "Dumbledore",
+        house: "Gryffindor",
+        pet: "Phoenix",
+        occupation: "Teacher",
+    },
+];
+
+//task 1
+function atGryffindor(arr) {
+    for (i of arr) {
+        const { firstName, lastName, house } = i;
+        if (house === "Gryffindor") {
+            console.log(`${firstName} ${lastName}`);
+        }
+    }
+}
+
+atGryffindor(hogwarts);
+
+//task 2
+function whoHasPet(arr) {
+    for (i of arr) {
+        const { firstName, lastName, pet, occupation } = i;
+        if (occupation === "Teacher" && pet) {
+            console.log(firstName, lastName);
+        }
+    }
+}
+console.log(`---------task2--------`);
+console.log(whoHasPet(hogwarts));
