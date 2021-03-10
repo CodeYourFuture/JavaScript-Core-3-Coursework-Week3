0c4e66a2c29b7fcdaf4cf5ee3a5390d7412783e0
Fatimohkuforiji1 <fatimohkuforiji@yahoo.com>
2021-03-10 14:32:52 +0000

    Answered all questions

--- exercises/exercise-2/exercise-2.js
+++ exercises/exercise-2/exercise-2.js
@@ -9,3 +9,24 @@ let hogwarts = [
     { firstName: "Minerva", lastName: "McGonagall", house: "Gryffindor", pet: null, occupation: "Teacher" },
     { firstName: "Albus", lastName: "Dumbledore", house: "Gryffindor", pet: "Phoenix", occupation: "Teacher" }
 ]
+//   console.log(harry, hermione, draco, cedric)
+//   console.log(harry.pet);
+
+  hogwarts.forEach(function(hogwart){
+      let {firstName, lastName,house, ...rest} = hogwart;
+      if (house === "Gryffindor") {
+           console.log(`${firstName} ${lastName} ${rest.pet}`)
+              
+  })
+  
+console.log("--------------------------------------")
+   // let [harry, hermione, draco, cedric,severus, filius, pomona, minerva, albus] = hogwarts;
+   // console.log(`${harry.firstName} ${harry.lastName}`)
+
+
+hogwarts.forEach(function (hogwart) {
+  let { firstName, lastName,occupation, pet} = hogwart;
+  if (occupation === "Teacher" && pet !==null) {
+    console.log(`${firstName} ${lastName} ${pet}`);
+  }
+});
--- exercises/exercise-3/exercise-3.js
+++ exercises/exercise-3/exercise-3.js
@@ -6,4 +6,16 @@
      { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.00},
      { itemName: "Hash Brown", quantity: 4, unitPrice: 0.40}
  ]
+ let total = 0;
+
+ console.log(`${"QTY"}\t\t${"ITEM".padEnd(20)}${"TOTAL"}`);
+ 
+ order.forEach(function(item){
+     let {itemName, quantity, unitPrice} = item
+     console.log(`${quantity}\t\t${itemName.padEnd(20)}${unitPrice}`)
+    total = total + unitPrice;
+    
+ })
+ console.log(total);
+
  
