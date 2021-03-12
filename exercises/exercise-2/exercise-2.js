e47afcc894e9bd752201ef0c01699dd8dd931e16
Reem Doukmak <remidoukmak@gmail.com>
2021-03-12 13:33:45 +0000

    completed exercises

--- exercises/exercise-2/exercise-2.js
+++ exercises/exercise-2/exercise-2.js
@@ -1,11 +1,85 @@
 let hogwarts = [
-    { firstName: "Harry", lastName: "Potter", house: "Gryffindor", pet: "Owl", occupation: "Student" },
-    { firstName: "Hermione", lastName: "Granger", house: "Gryffindor", pet: "Cat", occupation: "Student" },
-    { firstName: "Draco", lastName: "Malfoy", house: "Slytherin", pet: null, occupation: "Student" },
-    { firstName: "Cedric", lastName: "Diggory", house: "HufflePuff", pet: null, occupation: "Student" },
-    { firstName: "Severus", lastName: "Snape", house: "Slytherin", pet: null, occupation: "Teacher" },
-    { firstName: "Filius", lastName: "Flitwick", house: "Ravenclaw", pet: null, occupation: "Teacher" },
-    { firstName: "Pomona", lastName: "Sprout", house: "Hufflepuff", pet: null, occupation: "Teacher" },
-    { firstName: "Minerva", lastName: "McGonagall", house: "Gryffindor", pet: null, occupation: "Teacher" },
-    { firstName: "Albus", lastName: "Dumbledore", house: "Gryffindor", pet: "Phoenix", occupation: "Teacher" }
-]
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    house: "Gryffindor",
+    pet: "Owl",
+    occupation: "Student",
+  },
+  {
+    firstName: "Hermione",
+    lastName: "Granger",
+    house: "Gryffindor",
+    pet: "Cat",
+    occupation: "Student",
+  },
+  {
+    firstName: "Draco",
+    lastName: "Malfoy",
+    house: "Slytherin",
+    pet: null,
+    occupation: "Student",
+  },
+  {
+    firstName: "Cedric",
+    lastName: "Diggory",
+    house: "HufflePuff",
+    pet: null,
+    occupation: "Student",
+  },
+  {
+    firstName: "Severus",
+    lastName: "Snape",
+    house: "Slytherin",
+    pet: null,
+    occupation: "Teacher",
+  },
+  {
+    firstName: "Filius",
+    lastName: "Flitwick",
+    house: "Ravenclaw",
+    pet: null,
+    occupation: "Teacher",
+  },
+  {
+    firstName: "Pomona",
+    lastName: "Sprout",
+    house: "Hufflepuff",
+    pet: null,
+    occupation: "Teacher",
+  },
+  {
+    firstName: "Minerva",
+    lastName: "McGonagall",
+    house: "Gryffindor",
+    pet: null,
+    occupation: "Teacher",
+  },
+  {
+    firstName: "Albus",
+    lastName: "Dumbledore",
+    house: "Gryffindor",
+    pet: "Phoenix",
+    occupation: "Teacher",
+  },
+];
+// function hogwartsNames(hogwarts) {
+//   hogwarts.forEach(({ firstName, lastName, house } = person) => {
+//     if (house === "Gryffindor") {
+//       console.log(
+//         `My name is ${firstName} ${lastName} and I live in ${house}.);`
+//       );
+//     }
+//   });
+// }
+
+// hogwartsNames(hogwarts);
+
+function withPet(arr) {
+  arr.forEach(({ firstName, lastName, pet, occupation } = teacher) => {
+    if (occupation === "Teacher" && pet !== null) {
+      console.log(`My name is ${firstName} ${lastName}`);
+    }
+  });
+}
+withPet(hogwarts);
--- exercises/exercise-3/exercise-3.js
+++ exercises/exercise-3/exercise-3.js
@@ -1,9 +1,35 @@
- let order = [
-     { itemName: "Hot cakes", quantity: 1, unitPrice: 2.29},
-     { itemName: "Apple Pie", quantity: 2, unitPrice: 1.39},
-     { itemName: "Egg McMuffin", quantity: 1, unitPrice: 2.80},
-     { itemName: "Sausage McMuffin", quantity: 1, unitPrice: 3.00},
-     { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.00},
-     { itemName: "Hash Brown", quantity: 4, unitPrice: 0.40}
- ]
- 
+let order = [
+  { itemName: "Hot cakes", quantity: 1, unitPrice: 2.29 },
+  { itemName: "Apple Pie", quantity: 2, unitPrice: 1.39 },
+  { itemName: "Egg McMuffin", quantity: 1, unitPrice: 2.8 },
+  { itemName: "Sausage McMuffin", quantity: 1, unitPrice: 3.0 },
+  { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
+  { itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
+];
+
+// const order2 = [itemName, quantity, unitPrice];
+
+function takeoutItems(order) {
+  let total = 0;
+  let result = [];
+
+  order.forEach(({ itemName, quantity, unitPrice } = orderItem) => {
+    // console.log(itemName);
+    const price = unitPrice * quantity;
+    total += price;
+
+    const receipt = {
+      Quantity: quantity,
+      Name: itemName,
+      Total: price.toFixed(2),
+    };
+
+    result.push(receipt);
+
+    console.log(
+      `The item I want is ${itemName}. The quantity I need is ${quantity} and it costs £${unitPrice}.);`
+    );
+  });
+  console.log(`Total Price: ${total}`);
+}
+takeoutItems(order);
