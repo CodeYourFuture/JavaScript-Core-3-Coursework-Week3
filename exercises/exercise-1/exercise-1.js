c28399075578782bc01365061bbc58cd24cd56ec
alinosratipour <alinosratipour@gmail.com>
2021-03-30 02:03:56 +0100

    completed exercise-1

--- exercises/exercise-1/exercise-1.js
+++ exercises/exercise-1/exercise-1.js
@@ -4,8 +4,12 @@ const personOne = {
     favouriteFood: 'Spinach'
 }
 
-function introduceYourself(___________________________) {
-    console.log (`Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`); 
+let { name,age ,favouriteFood} = personOne;
+
+function introduceYourself(){
+  console.log(
+    `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
+  );
 }
 
 introduceYourself(personOne);
