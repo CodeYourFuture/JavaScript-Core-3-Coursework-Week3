f9443cb3f3e63f811ad298919419ac7183e6f5e3
BenyamWorku <benworku@gmail.com>
2021-04-07 13:28:24 +0100

    changes made

--- exercises/exercise-1/exercise-1.js
+++ exercises/exercise-1/exercise-1.js
@@ -1,12 +1,25 @@
+// const personOne = {
+//   name: "Popeye",
+//   age: 34,
+//   favouriteFood: "Spinach",
+// };
+
+// function introduceYourself(___________________________) {
+//   console.log(
+//     `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
+//   );
+// }
+
+// introduceYourself(personOne);
 const personOne = {
-  name: "Popeye",
+  firstName: "Popeye",
   age: 34,
   favouriteFood: "Spinach",
 };
-
-function introduceYourself(___________________________) {
+let { firstName, age, favouriteFood } = personOne;
+function introduceYourself(personOne) {
   console.log(
-    `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
+    `Hello, my name is ${firstName}. I am ${age} years old and my favourite food is ${favouriteFood}.`
   );
 }
 
