465ea678b24386026ca9088097de0ad7426c810f
Kara <kara.mh191@gmail.com>
2021-04-07 15:55:22 +0100

    Exercise 3 Complete
    Exercise 3 Complete

--- exercises/exercise-3/exercise-3.js
+++ exercises/exercise-3/exercise-3.js
@@ -6,3 +6,25 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
   { itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
 ];
+
+function printReceipt() {
+  console.log('---------------------------------------------');
+  console.log('QTY\t\tITEM\t\t\tTOTAL');
+  console.log('---------------------------------------------');
+  let finalTotal = 0;
+  for (customerOrder of order) {
+    let { quantity, itemName, unitPrice } = customerOrder;
+    let costOfItems = unitPrice*quantity;
+    if (itemName.length > 13)  {
+      console.log(`${quantity}\t\t${itemName}\t£${costOfItems.toFixed(2)}`);
+    } else {
+      console.log(`${quantity}\t\t${itemName}\t\t£${costOfItems.toFixed(2)}`);
+    }
+    finalTotal += costOfItems;
+  }
+  console.log('---------------------------------------------');
+  console.log(`Total:\t\t£${finalTotal}`);
+  console.log('---------------------------------------------');
+};
+
+printReceipt();
