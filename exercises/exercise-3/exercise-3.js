8d3204ce6736e025bd19c2a501dce8d73a973a01
Deniz Kelemence <dkelemence@gmail.com>
2021-04-07 23:42:28 +0100

    Update exercise-3.js

--- exercises/exercise-3/exercise-3.js
+++ exercises/exercise-3/exercise-3.js
@@ -1,10 +1,10 @@
 let order = [
   { itemName: "Hot cakes", quantity: 1, unitPrice: 2.29 },
-  { itemName: "Apple Pie", quantity: 2, unitPrice: 2.78 },
+  { itemName: "Apple Pie", quantity: 2, unitPrice: 1.39 },
   { itemName: "Egg McMuffin", quantity: 1, unitPrice: 2.8 },
   { itemName: "Sausage McMuffin", quantity: 1, unitPrice: 3.0 },
-  { itemName: "Hot Coffee", quantity: 2, unitPrice: 2.0 },
-  { itemName: "Hash Brown", quantity: 4, unitPrice: 1.6 },
+  { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
+  { itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
 ];
 
 // ## Expected result
@@ -26,7 +26,8 @@ function totalCost(array){
 array.forEach(element => {
   let {itemName, quantity, unitPrice} = element;
   console.log(`${quantity}       ${itemName}           ${unitPrice.toFixed(2)}`)
-  total += unitPrice;
+  let costOfItems = unitPrice*quantity;
+  total += costOfItems;
 });
 console.log('Total: ', total.toFixed(2));
 }
