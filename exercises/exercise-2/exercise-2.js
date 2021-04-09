47579d8ac8cba71c32a9c19e32520e3ecd1ba71e
rfloresram <74856999+rfloresram@users.noreply.github.com>
2021-04-09 18:26:55 +0200

    Exercises

--- exercises/exercise-2/exercise-2.js
+++ exercises/exercise-2/exercise-2.js
@@ -1,72 +1,48 @@
 let hogwarts = [
-  {
-    firstName: "Harry",
-    lastName: "Potter",
-    house: "Gryffindor",
-    pet: "Owl",
-    occupation: "Student",
-  },
-  {
-    firstName: "Ron",
-    lastName: "Weasley",
-    house: "Gryffindor",
-    pet: "Scabbers",
-    occupation: "Student",
-  },
-  {
-    firstName: "Hermione",
-    lastName: "Granger",
-    house: "Gryffindor",
-    pet: "Cat",
-    occupation: "Student",
-  },
-  {
-    firstName: "Draco",
-    lastName: "Malfoy",
-    house: "Slytherin",
-    pet: null,
-    occupation: "Student",
-  },
-  {
-    firstName: "Cedric",
-    lastName: "Diggory",
-    house: "HufflePuff",
-    pet: null,
-    occupation: "Student",
-  },
-  {
-    firstName: "Severus",
-    lastName: "Snape",
-    house: "Slytherin",
-    pet: null,
-    occupation: "Teacher",
-  },
-  {
-    firstName: "Filius",
-    lastName: "Flitwick",
-    house: "Ravenclaw",
-    pet: null,
-    occupation: "Teacher",
-  },
-  {
-    firstName: "Pomona",
-    lastName: "Sprout",
-    house: "Hufflepuff",
-    pet: null,
-    occupation: "Teacher",
-  },
-  {
-    firstName: "Minerva",
-    lastName: "McGonagall",
-    house: "Gryffindor",
-    pet: null,
-    occupation: "Teacher",
-  },
-  {
-    firstName: "Albus",
-    lastName: "Dumbledore",
-    house: "Gryffindor",
-    pet: "Phoenix",
-    occupation: "Teacher",
-  },
-];
+    { firstName: "Harry", lastName: "Potter", house: "Gryffindor", pet: "Owl", occupation: "Student" },
+    { firstName: "Ron", lastName: "Weasley", house: "Gryffindor", pet: "Scabbers", occupation: "Student" },
+    { firstName: "Hermione", lastName: "Granger", house: "Gryffindor", pet: "Cat", occupation: "Student" },
+    { firstName: "Draco", lastName: "Malfoy", house: "Slytherin", pet: null, occupation: "Student" },
+    { firstName: "Cedric", lastName: "Diggory", house: "HufflePuff", pet: null, occupation: "Student" },
+    { firstName: "Severus", lastName: "Snape", house: "Slytherin", pet: null, occupation: "Teacher" },
+    { firstName: "Filius", lastName: "Flitwick", house: "Ravenclaw", pet: null, occupation: "Teacher" },
+    { firstName: "Pomona", lastName: "Sprout", house: "Hufflepuff", pet: null, occupation: "Teacher" },
+    { firstName: "Minerva", lastName: "McGonagall", house: "Gryffindor", pet: null, occupation: "Teacher" },
+    { firstName: "Albus", lastName: "Dumbledore", house: "Gryffindor", pet: "Phoenix", occupation: "Teacher" }
+]
+
+// In `exercise-2.js` write a program that will take the `hogwarts` array as input and display the names of the 
+// people who belong to the Gryffindor house.
+
+function hogwartsHouse(hogwarts) {
+     hogwarts.map(student=> {
+
+        const {firstName, lastName, house} = student;
+
+         if (house === "Gryffindor") {
+
+             console.log(`${firstName} ${lastName}`)
+         };
+     })
+ }
+ 
+hogwartsHouse(hogwarts)
+
+// - In `exercise-2.js` write a program that will take the `hogwarts` array as input and display the names 
+// of teachers who have pets.
+
+ let [{firstName, lastName, house, pet, occupation}] = hogwarts
+
+ function nameOfTeachers(hogwarts) {
+
+     hogwarts.map((student)=> {
+
+          const {firstName, lastName, pet, occupation} = student;
+          if (pet != null & occupation === "Teacher") {
+
+            console.log(`${firstName} ${lastName}`)
+        };
+    })
+}
+nameOfTeachers(hogwarts)
+
--- exercises/exercise-3/exercise-3.js
+++ exercises/exercise-3/exercise-3.js
@@ -6,3 +6,21 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
   { itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
 ];
+
+
+function groceryReceipt(order){
+  console.log(`#    PRODUCTS         TOTAL AMOUNT`);
+  let total = 0
+
+  order.forEach((product) => {
+
+      let { itemName, quantity, unitPrice } = product;
+
+      total += quantity * unitPrice;
+      
+      console.log(`${quantity}    ${itemName}        £${(
+        quantity * unitPrice).toFixed(2)}`);
+    });
+    console.log(`\nTOTAL: ${total}`);
+}
+groceryReceipt(order)
