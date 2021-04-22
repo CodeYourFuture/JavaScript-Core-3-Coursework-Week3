a33710411bf3147ca21354f065eebefa7d1c17f2
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-04-22 09:03:49 +0200

    add total items

--- exercises/exercise-3/exercise-3.js
+++ exercises/exercise-3/exercise-3.js
@@ -9,21 +9,26 @@ let order = [
 
 let [{ itemName, quantity, unitPrice }] = order;
 
-console.log(`QTY\tITEM\t\t\t\tTOTAL`);
+console.log(`QTY\tITEM\t\t\t\tTOTAL`);  // Setting up the headers with tab spacing.
 
-let total = 0;
+let totalPrice = 0; //  Variable to store the total price.
+let totalItems = 0; //  Variable to store the total items.
 
-for (value in order) {
-  //  console.log(`value of quantity: ${order[value].quantity}`)
-  console.log(`${order[value].quantity}\t${order[value].itemName.padEnd(16, " ")}\t \t${order[value].unitPrice * order[value].quantity}`);  
-  // console.log(`Total1: ${total}`)
-  total += order[value].unitPrice * order[value].quantity;
-  // console.log(`Total2: ${total}`)
+for (values in order) {
+  //  console.log(`values of quantity: ${order[values].quantity}`)
+  //  Values returns/ an object/s, I access the keys and values using dot notaion and the destructured keys.
+  console.log(`${order[values].quantity}\t${order[values].itemName.padEnd(16, " ")}\t \t${order[values].unitPrice * order[values].quantity}`);
+  // console.log(`Total1: ${total}`)  //  Before increment
+  totalPrice += order[values].unitPrice * order[values].quantity;
+  totalItems += order[values].quantity
+  /* adds 1 values to another and stores it in totalprice => takes the new values and add the next values to it and stores it in totalPrice...*/
+  // console.log(`Total2: ${totalPrice}`) //  After increment
 };
 
-// let total = order.reduce(someFunction, 0);
+// let totalPrice = order.reduce(someFunction, 0);
 // function someFunction (previousNumber, currentNumber) {
 //   return previousNumber + currentNumber.unitPrice * currentNumber.quantity;
 // };
 
-console.log(`Total: ${total}`);
+console.log(`Total Price: ${totalPrice}`);
+console.log(`Total Items: ${totalItems}`);
