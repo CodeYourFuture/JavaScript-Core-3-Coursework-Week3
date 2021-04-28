b09072238d3185798d965e04fe905e4a8868287f
Bradley Mubenga <bradleymubenga777@gmail.com>
2021-04-28 14:24:41 +0200

    Exercise 1 Complete

--- exercises/exercise-1/exercise-1.js
+++ exercises/exercise-1/exercise-1.js
@@ -1,8 +1,10 @@
-const personOne = {
+let personOne = {
   name: "Popeye",
   age: 34,
   favouriteFood: "Spinach",
 };
+
+let { name, age, favouriteFood } = personOne;
 
 function introduceYourself(___________________________) {
   console.log(
