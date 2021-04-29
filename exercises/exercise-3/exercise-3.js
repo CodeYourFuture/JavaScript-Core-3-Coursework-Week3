ed051dc804eb611d62bc9982adc2ba5a271f1d9d
Bradley Mubenga <bradleymubenga777@gmail.com>
2021-04-29 12:20:50 +0200

    Exercise 3 Completed

--- exercises/exercise-3/exercise-3.js
+++ exercises/exercise-3/exercise-3.js
@@ -6,3 +6,21 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
   { itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
 ];
+
+function printOrders(arrayOfOrders){
+  let [a, b, c, d, e, f] = arrayOfOrders;
+  const tabRegEx = /\t/;
+  const newLRegEx = /\n/;
+
+  console.log(`
+  QTY             ITEM                TOTAL
+  ${a.quantity}   ${a.itemName}       ${(a.unitPrice * a.quantity).toFixed(2)}
+  ${b.quantity}   ${b.itemName}       ${(b.unitPrice * b.quantity).toFixed(2)}
+  ${c.quantity}   ${c.itemName}       ${(c.unitPrice * c.quantity).toFixed(2)}
+  ${d.quantity}   ${d.itemName}       ${(d.unitPrice * d.quantity).toFixed(2)}
+  ${e.quantity}   ${e.itemName}       ${(e.unitPrice * e.quantity).toFixed(2)}
+  ${f.quantity}   ${f.itemName}       ${(f.unitPrice * f.quantity).toFixed(2)}
+  `);
+}
+
+printOrders(order);
