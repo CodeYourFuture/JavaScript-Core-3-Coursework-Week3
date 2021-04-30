7e5b7934e1bac1efeb878d2463217c545722ec11
KudahShambare <kudahshambare263@gmail.com>
2021-04-30 15:26:42 +0200

    Exercise 3 Done

--- exercises/exercise-3/exercise-3.js
+++ exercises/exercise-3/exercise-3.js
@@ -6,3 +6,14 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
   { itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
 ];
+function shoppingOrder(arr){
+  let total=0;
+  for(let i=0;i<arr.length;i++){
+    let{itemName:item,quantity:number,unitPrice:price}=arr[i];
+    let allPrice=number*price
+    total+=allPrice;
+    console.log(`${number} ${item}         ${allPrice}`)
+  }
+  console.log(total);
+}
+shoppingOrder(order);
